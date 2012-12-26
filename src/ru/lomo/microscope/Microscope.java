01df4e0cdb57c70c6517af7392b48e7bc2cd6ae3
Anton Skshidlevsky <meefik@gmail.com>
2012-12-27 02:19:45 +0400

    1.0.4

--- src/ru/lomo/microscope/Microscope.java
+++ src/ru/lomo/microscope/Microscope.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,19 +16,24 @@ import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
 import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.FloatMath;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 
-public class Microscope extends Activity {
+public class Microscope extends Activity implements OnTouchListener {
 
 	static Handler handler;
 	private static TextView logView;
@@ -38,6 +44,24 @@ public class Microscope extends Activity {
 	private boolean isActive = false;
 	
 	private boolean rotateState = false;
+	
+	// These matrices will be used to move and zoom image
+	Matrix matrix = new Matrix();
+	Matrix savedMatrix = new Matrix();
+	
+	// We can be in one of these 3 states
+    static final int NONE = 0;
+	static final int DRAG = 1;
+	static final int ZOOM = 2;
+	int mode = NONE;
+	
+	// Remember some things for zooming
+	PointF start = new PointF();
+	PointF mid = new PointF();
+	float oldDist = 1f;
+	
+    //creates a Gesture Detector  
+    private GestureDetector gd; 
 
 	static {
 		System.loadLibrary("microscope");
@@ -158,7 +182,6 @@ public class Microscope extends Activity {
 		final Button flipBtn = (Button) findViewById(R.id.flipBtn);
 		flipBtn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Matrix matrix = new Matrix();
 				float rotate;
 				if (rotateState) {
 					rotate = 0f;
@@ -167,11 +190,25 @@ public class Microscope extends Activity {
 					rotate = 180f;
 					rotateState = true;
 				}
-				matrix.postRotate(rotate, iv.getDrawable().getBounds()
-						.width() / 2, iv.getDrawable().getBounds().height() / 2);
+				//matrix.postRotate(rotate, iv.getDrawable().getBounds().width()/2, iv.getDrawable().getBounds().height()/2);
+				matrix.setRotate(rotate, iv.getDrawable().getBounds().width()/2, iv.getDrawable().getBounds().height()/2);
 				iv.setImageMatrix(matrix);
 			}
 		});
+		
+		iv.setOnTouchListener(this);
+		
+        //initialize the Gesture Detector  
+        gd = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener()  
+        {  
+            @Override  
+            public boolean onDoubleTap(MotionEvent e)  
+            {  
+            	matrix.reset();
+            	savedMatrix.reset();
+                return false;  
+            }  
+        });  
 	}
 
 	@Override
@@ -198,6 +235,73 @@ public class Microscope extends Activity {
 	public void onPause() {
 		super.onPause();
 		isActive = false;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+	      ImageView view = (ImageView) v;
+
+	      // Handle touch events here...
+	      switch (event.getAction() & MotionEvent.ACTION_MASK) {
+	      case MotionEvent.ACTION_DOWN:
+	         savedMatrix.set(matrix);
+	         start.set(event.getX(), event.getY());
+	         mode = DRAG;
+	         break;
+	      case MotionEvent.ACTION_POINTER_DOWN:
+	         oldDist = spacing(event);
+	         if (oldDist > 10f) {
+	            savedMatrix.set(matrix);
+	            midPoint(mid, event);
+	            mode = ZOOM;
+	         }
+	         break;
+	      case MotionEvent.ACTION_UP:
+	      case MotionEvent.ACTION_POINTER_UP:
+	         mode = NONE;
+	         break;
+	      case MotionEvent.ACTION_MOVE:
+	         if (mode == DRAG) {
+	            matrix.set(savedMatrix);
+	            //float newx = event.getX() - start.x;
+	            //float newy = event.getY() - start.y;
+	            //showStatus(String.valueOf(newx)+" "+String.valueOf(newy));
+	            matrix.postTranslate(event.getX() - start.x,
+	                  event.getY() - start.y);
+	            
+	         }
+	         else if (mode == ZOOM) {
+	            float newDist = spacing(event);
+	            if (newDist > 10f) {
+	               matrix.set(savedMatrix);
+	               float scale = newDist / oldDist;
+	               matrix.postScale(scale, scale, mid.x, mid.y);
+	            }
+	         }
+	         break;
+	      }
+
+	      view.setImageMatrix(matrix);
+	      
+	      gd.onTouchEvent(event);
+	      
+	      return true;
+	}
+	
+	/** Determine the space between the first two fingers */
+	@SuppressLint("NewApi")
+	private float spacing(MotionEvent event) {
+	    float x = event.getX(0) - event.getX(1);
+	    float y = event.getY(0) - event.getY(1);
+	    return FloatMath.sqrt(x * x + y * y);
+	}
+
+	/** Calculate the mid point of the first two fingers */
+	@SuppressLint("NewApi")
+	private void midPoint(PointF point, MotionEvent event) {
+		float x = event.getX(0) + event.getX(1);
+	    float y = event.getY(0) + event.getY(1);
+	    point.set(x / 2, y / 2);
 	}
 
 }
